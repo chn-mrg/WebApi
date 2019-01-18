3fa92db5bee8cada945c9defd4a5aee430d42b49
CHNMrG <chnmrg@gmail.com>
2019-01-18 17:56:23 +0700

    清空

--- Application/Home/Controller/IndexController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
-        echo "歡迎來到本頁面，招聘郵箱：thmrsix@gmail.com";
-        //$this->ajaxReturn(array('code'=>404,'msg'=>'歡迎來到本頁面'),'JSON');
-    }
-}

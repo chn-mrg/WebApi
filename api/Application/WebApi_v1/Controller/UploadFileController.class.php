e864451ed717138cc45b1c9f6a9ebb369d1eae62
CHNMrG <chnmrg@gmail.com>
2019-01-20 18:37:31 +0700

    用戶上傳文件

--- api/Application/WebApi_v1/Controller/UploadFileController.class.php
+++ api/Application/WebApi_v1/Controller/UploadFileController.class.php
@@ -12,43 +12,58 @@ namespace WebApi_v1\Controller;
 class UploadFileController extends UserBaseController
 {
     public function uploadImg(){
-        $FilePath = "Img/" . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
-        $result = self::UploadImgFunction($_FILES['file'],$FilePath);
-        if($result['result']){
-            self::returnAjax(200,array('key'=>$result['url'],'url'=>self::ResourceAwsS3Url($result['url'])));
+        $userInfo = self::getUserInfo();
+        $level = $userInfo?self::level($userInfo['experience']):0;
+        if($level['level']>=10) {
+            $FilePath = "Img/User_".$userInfo['user_id']."/" . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
+            $result = self::UploadImgFunction($_FILES['file'], $FilePath);
+            if ($result['result']) {
+                self::returnAjax(200, array('key' => $result['url'], 'url' => self::ResourceAwsS3Url($result['url'])));
+            }
+            self::returnAjax(301, array('reason' => $result['reason']));
         }
-        self::returnAjax(301,array('reason'=>$result['reason']));
+        self::returnAjax(301, array('reason' => '用戶等級不足10級'));
     }
 
     public function uploadImgBatch(){
-        foreach ($_FILES['file']['tmp_name'] as $k=>$v){
-            $FilePath = "Img/" . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
-            $result = self::UploadImgFunction(array('tmp_name'=>$v),$FilePath);
-            if($result['result']){
-                $data[] = array('key'=>$result['url'],'url'=>self::ResourceAwsS3Url($result['url']));
+        $userInfo = self::getUserInfo();
+        $level = $userInfo?self::level($userInfo['experience']):0;
+        if($level['level']>=10) {
+            foreach ($_FILES['file']['tmp_name'] as $k => $v) {
+                $FilePath = "Img/" . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
+                $result = self::UploadImgFunction(array('tmp_name' => $v), $FilePath);
+                if ($result['result']) {
+                    $data[] = array('key' => $result['url'], 'url' => self::ResourceAwsS3Url($result['url']));
+                }
             }
+            if (is_array($data)) {
+                self::returnAjax(200, $data);
+            }
+            self::returnAjax(301);
         }
-        if(is_array($data)){
-            self::returnAjax(200,$data);
-        }
-        self::returnAjax(301);
+        self::returnAjax(301, array('reason' => '用戶等級不足10級'));
     }
 
     public function uploadMp4(){
-        $FilePath = "TemMp4/" . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
-        $result = self::UpLoadMp4Function($_FILES['file'],$FilePath);
-        if($result['result']){
-            self::returnAjax(200,array('key'=>$result['url'],'url'=>self::ResourceAwsS3Url($result['url'])));
+        $userInfo = self::getUserInfo();
+        $level = $userInfo?self::level($userInfo['experience']):0;
+        if($level['level']>=10) {
+            $FilePath = "TemMp4/User_".$userInfo['user_id']."/"  . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
+            $result = self::UpLoadMp4Function($_FILES['file'], $FilePath);
+            if ($result['result']) {
+                self::returnAjax(200, array('key' => $result['url'], 'url' => self::ResourceAwsS3Url($result['url'])));
+            }
+            self::returnAjax(301, array('reason' => $result['reason']));
         }
-        self::returnAjax(301,array('reason'=>$result['reason']));
+        self::returnAjax(301, array('reason' => '用戶等級不足10級'));
     }
 
-    public function uploadMp3(){
-        $FilePath = "Mp3/" . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
-        $result = self::UpLoadMp3Function($_FILES['file'],$FilePath);
-        if($result['result']){
-            self::returnAjax(200,array('key'=>$result['url'],'url'=>self::ResourceAwsS3Url($result['url'])));
-        }
-        self::returnAjax(301,array('reason'=>$result['reason']));
-    }
+//    public function uploadMp3(){
+//        $FilePath = "Mp3/" . date('Ymd') . "/" . date('YmdHis') . mt_rand(10000000, 99999999);
+//        $result = self::UpLoadMp3Function($_FILES['file'],$FilePath);
+//        if($result['result']){
+//            self::returnAjax(200,array('key'=>$result['url'],'url'=>self::ResourceAwsS3Url($result['url'])));
+//        }
+//        self::returnAjax(301,array('reason'=>$result['reason']));
+//    }
 }
